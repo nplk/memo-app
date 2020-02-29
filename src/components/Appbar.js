be575c99793f22e4096530ad86066bee85395388
nplk <nplk789@gmail.com>
2020-02-29 22:34:36 +0900

    Refactoring MemoList.

---
+++ src/components/Appbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265356',
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+});
+
+
+class Appbar extends React.Component {
+  render() {
+    return (
+      <View style={styles.appbar}>
+        <Text style={styles.appbarTitle}>MEMOT</Text>
+      </View>
+    );
+  }
+}
+
+export default Appbar;
